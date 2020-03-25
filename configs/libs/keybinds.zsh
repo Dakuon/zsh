14a83b3a542ffea880c48cc82e275c00581c7aff
Tor Egil Isaksen <toregil.isaksen@phonero.no>
2020-03-25 12:38:02 +0100

    fix prompt up/down search keybind

--- configs/libs/keybinds.zsh
+++ configs/libs/keybinds.zsh
@@ -3,10 +3,10 @@ autoload -U up-line-or-beginning-search
 autoload -U down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
-bindkey "${terminfo[kpp]}" up-line-or-history
-bindkey "${terminfo[knp]}" down-line-or-history
-bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
-bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search
+bindkey "^[[B" down-line-or-beginning-search
+bindkey "^[[1;5A" up-line-or-history
+bindkey "^[[1;5B" down-line-or-history
 
 bindkey "^[[H" beginning-of-line
 bindkey "^[[F" end-of-line
