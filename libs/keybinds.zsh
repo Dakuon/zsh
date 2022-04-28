25e489bbbb8cbe71730ad5939149b0e7091fa179
Tor Egil Isaksen <toregil.isaksen@phonero.no>
2022-04-28 10:02:12 +0200

    fix move f/b keybinds for osx

--- libs/keybinds.zsh
+++ libs/keybinds.zsh
@@ -92,14 +92,27 @@ bindkey -M emacs '^[[3;5~' kill-word
 bindkey -M viins '^[[3;5~' kill-word
 bindkey -M vicmd '^[[3;5~' kill-word
 
-# [Ctrl-RightArrow] - move forward one word
-bindkey -M emacs '^[[1;5C' forward-word
-bindkey -M viins '^[[1;5C' forward-word
-bindkey -M vicmd '^[[1;5C' forward-word
-# [Ctrl-LeftArrow] - move backward one word
-bindkey -M emacs '^[[1;5D' backward-word
-bindkey -M viins '^[[1;5D' backward-word
-bindkey -M vicmd '^[[1;5D' backward-word
+if [ "$(uname)" == "Darwin" ]; then
+  # [Option-RightArrow] - move forward one word
+  bindkey -M emacs "^[^[[C" forward-word
+  bindkey -M viins "^[^[[C" forward-word
+  bindkey -M vicmd "^[^[[C" forward-word
+
+  # [Option-LeftArrow] - move backward one word
+  bindkey -M emacs "^[^[[D" backward-word
+  bindkey -M viins "^[^[[D" backward-word
+  bindkey -M vicmd "^[^[[D" backward-word
+else
+  # [Ctrl-RightArrow] - move forward one word
+  bindkey -M emacs '^[[1;5C' forward-word
+  bindkey -M viins '^[[1;5C' forward-word
+  bindkey -M vicmd '^[[1;5C' forward-word
+  # [Ctrl-LeftArrow] - move backward one word
+  bindkey -M emacs '^[[1;5D' backward-word
+  bindkey -M viins '^[[1;5D' backward-word
+  bindkey -M vicmd '^[[1;5D' backward-word
+fi
+
 
 bindkey '\ew' kill-region                             # [Esc-w] - Kill from the cursor to the mark
 bindkey -s '\el' 'l\n'                               # [Esc-l] - run command: ls
