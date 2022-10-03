78e26e3c1685a4032364561edad9cff5ef349835
Tor Egil Isaksen <toregil.isaksen@phonero.no>
2022-10-03 08:31:26 +0200

    docker alias for podman if no docker

--- completion/podman.zsh
+++ completion/podman.zsh
@@ -2,4 +2,6 @@ if [[ ! -f ~/.completion/_podman || "$(which podman)" -nt ~/.completion/_podman 
   podman completion zsh > ~/.completion/_podman
 fi
 
-alias docker=podman
+if [ ! $commands[docker] ]; then
+  alias docker=podman
+fi
