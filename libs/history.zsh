08904e2c80a2e3d11668fb8e26b4f87a0b3cb8fd
Tor Egil Isaksen <tor.egil.isaksen@gmail.com>
2023-08-04 09:05:41 +0200

    add fzf history alias

--- libs/history.zsh
+++ libs/history.zsh
@@ -42,3 +42,7 @@ setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_verify            # show command with history expansion to user before running it
 setopt inc_append_history     # add commands to HISTFILE in order of execution
 setopt share_history          # share command history data
+
+if [ $commands[fzf] ]; then
+  alias hist='print -z $(history | tac | fzf --tiebreak=index | sed "s|^ \+||" | cut -d" " -f5-)'
+fi
