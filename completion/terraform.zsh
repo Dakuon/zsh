42389dd360f3485db1461dc03a7fe6b2f39c9310
Tor Egil Isaksen <tor.egil.isaksen@gmail.com>
2023-10-17 10:16:44 +0200

    add tf alias for opentofu

--- completion/terraform.zsh
+++ completion/terraform.zsh
@@ -1,3 +1,7 @@
-complete -o nospace -C $(which terraform) terraform tf
+complete -o nospace -C $(which terraform) terraform tf tofu
+
+if [ $commands[tofu] ]; then
+  alias terraform=tofu
+fi
 
 alias tf=terraform
